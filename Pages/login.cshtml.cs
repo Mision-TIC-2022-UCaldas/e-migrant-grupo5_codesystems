7c5f5a00eecb10d0310720da4b69d7f74ffd9479
jean c <jeancgoyeneche@gmail.com>
2021-11-29 01:05:43 -0500

    first commit

---
+++ Pages/login.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace web.App.frontend.Pages
+{
+    public class loginModel : PageModel
+    {
+        private readonly ILogger<loginModel> _logger;
+
+        public loginModel(ILogger<loginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
